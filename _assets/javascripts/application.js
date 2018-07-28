e9262d21aa1c366462e9c64e739aa7c8f2bca755
Daria Woźnicka <woznicka.daria@gmail.com>
2018-07-28 17:06:36 +0200

    desktop home ready

--- _assets/javascripts/application.js
+++ _assets/javascripts/application.js
@@ -1,3 +1,25 @@
 $(document).ready(function() {
+    var $calendar = $('.home-calendar-events');
 
+    if ($calendar.length) {
+        $calendar.slick({
+            slidesToShow: 6,
+            slidesToScroll: 6,
+            draggable: false,
+            vertical: true,
+            infinite: false,
+            appendArrows: $('.events-arrows'),
+            prevArrow: '<button type="button" class="slick-prev">Poprzednie</button>',
+            nextArrow: '<button type="button" class="slick-next">Następne</button>',
+            responsive: [
+                {
+                  breakpoint: 1200,
+                  settings: {
+                    slidesToShow: 4,
+                    slidesToScroll: 4
+                  }
+                }
+            ]
+        });
+    }
 });
