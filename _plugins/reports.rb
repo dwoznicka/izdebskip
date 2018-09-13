84facd46c203a26651899c0207343ce14bea70e0
Daria Woznicka <woznicka.daria@gmail.com>
2018-09-14 00:46:50 +0200

    fotorlacje (#1)
    * fotorlacje
    
    * update fotorelacja 7.09.2018
    
    * added fb_og for reports
    
    * added real photos to fotoreport

---
+++ _plugins/reports.rb
@@ -0,0 +1,39 @@
+require 'active_support/inflector'
+
+module Jekyll
+  class ReportIndex < Page
+    def initialize(site, base, dir, report = nil)
+      @site = site
+      @base = base
+      @dir = dir
+      @name = 'index.html'
+      self.process(@name)
+      self.read_yaml(File.join(base, '_layouts'), "#{site.config['news_page_layout']}.html")
+      self.data['report'] = report
+      self.data['title'] = "#{report['title']} | Relacja"
+      self.data['description'] = report['title']
+      self.data['image_index_url'] = report['image_url']
+    end
+  end
+  
+  class IntegrationGenerator < Generator
+    safe true
+    
+    def generate(site)
+      dir = site.config['news_path'] || '/news'
+      site.data['news_urls'] = []
+      if site.layouts.key? site.config['news_page_layout']
+        reports = site.data['news']
+        reports = reports.reject { |r| !r['is_report'] }
+        reports.each do |report|
+          report_id = report['id']
+          permalink = report_id['permalink'] ? report_id['permalink'] : report_id.to_s
+          url = File.join(dir, permalink.parameterize)
+          url = "#{url.chomp('/')}/"
+          site.pages << ReportIndex.new(site, site.source, url, report)
+          site.data['news_urls'] << url
+        end
+      end
+    end
+  end
+end
