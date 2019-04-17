dbad33726207f6f2529cb030b06736897b4ba02a
Daria Woźnicka <woznicka.daria@gmail.com>
2019-04-18 00:43:22 +0100

    new calendar wip

--- _assets/javascripts/application.js
+++ _assets/javascripts/application.js
@@ -162,23 +162,14 @@ $(document).ready(function() {
 
     if ($calendar.length) {
         $calendar.slick({
-            slidesToShow: 6,
-            slidesToScroll: 6,
+            slidesToShow: 4,
+            slidesToScroll: 4,
             draggable: false,
             vertical: true,
             infinite: false,
             appendArrows: $('.events-arrows'),
             prevArrow: '<button type="button" class="slick-prev">Poprzednie</button>',
             nextArrow: '<button type="button" class="slick-next">Następne</button>',
-            responsive: [
-                {
-                  breakpoint: 1200,
-                  settings: {
-                    slidesToShow: 4,
-                    slidesToScroll: 4
-                  }
-                }
-            ]
         });
     }
 
